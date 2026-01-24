21b1c1785b23f320b499d7a7b592b0901991d390
charlieforward9 <charlesrichardsonusa@gmail.com>
2026-01-24 02:24:33 -0500

    feat(operators): add controller configuration to DeckRendererOp and Views
    Add configurable controller options for deck.gl interactivity:
    
    DeckRendererOp:
    - Add controller CompoundPropsField with scrollZoom, dragPan, dragRotate,
      doubleClickZoom, touchZoom, touchRotate, keyboard, and inertia options
    - Defaults to true (all interactions enabled) when not configured
    
    View operators (MapViewOp, GlobeViewOp, etc.):
    - Add controller BooleanField to enable/disable user interaction per view
    
    This allows fine-grained control over map interactions from the node editor.

--- noodles-editor/src/noodles/operators.ts
+++ noodles-editor/src/noodles/operators.ts
@@ -3500,6 +3500,17 @@ export class DeckRendererOp extends Operator<DeckRendererOp> {
       views: new ListField(new ViewField()),
       widgets: new ListField(new WidgetField(), { showByDefault: false }),
       layerFilter: new FunctionField(() => true, { showByDefault: false }),
+      // Controller configuration for deck.gl interactivity
+      controller: new CompoundPropsField({
+        scrollZoom: new BooleanField(true),
+        dragPan: new BooleanField(true),
+        dragRotate: new BooleanField(true),
+        doubleClickZoom: new BooleanField(true),
+        touchZoom: new BooleanField(true),
+        touchRotate: new BooleanField(true),
+        keyboard: new BooleanField(true),
+        inertia: new BooleanField(true),
+      }, { optional: true, showByDefault: false }),
       // TODO: We need a nullable field. This should be a nullable (intentionally empty), or a compound object below.
       // TODO: Nullable fields need to be disable-able from the UI so their values can be cleared.
       basemap: new UnknownField(
@@ -3535,6 +3546,7 @@ export class DeckRendererOp extends Operator<DeckRendererOp> {
     basemap,
     views,
     layerFilter,
+    controller,
   }: ExtractProps<typeof this.inputs>): ExtractProps<typeof this.outputs> {
     // Validate the ViewState to ensure lat/lng are within valid bounds
     validateViewState(viewState)
@@ -3554,6 +3566,8 @@ export class DeckRendererOp extends Operator<DeckRendererOp> {
       viewState: { ...basemapViewState, ...viewState },
       layerFilter,
       widgets,
+      // Include controller config if provided - use true as default for interactivity
+      controller: controller && Object.keys(controller).length > 0 ? controller : true,
     }
 
     // Prefer viewState values when using a basemap
@@ -3583,6 +3597,8 @@ export class DeckRendererOp extends Operator<DeckRendererOp> {
 // Base view fields that apply to all view types
 function createBaseViewFields() {
   return {
+    // Controller enables user interaction (pan, zoom, rotate)
+    controller: new BooleanField(true),
     x: new NumberField(0, { showByDefault: false }),
     y: new NumberField(0, { showByDefault: false }),
     width: new StringField('100%'),
