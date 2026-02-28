f3eb5855e8fe09586fe08c2752515606aa25290b
Adam Krebs <akre54@users.noreply.github.com>
2026-02-28 15:01:54 -0500

    Revert "enhancement: Enable source maps for prod builds (#284)" (#317)
    This reverts commit 6bb7fc0ad20f7608003df6a0a36fb52a5b7b734a.

--- noodles-editor/vite.config.js
+++ noodles-editor/vite.config.js
@@ -101,11 +101,6 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: mode === 'development' ? '/' : '/app/',
-    build: {
-      // CF_PAGES=1 is set automatically by Cloudflare Pages; disable sourcemaps there
-      // due to the 25 MiB per-file limit. GitHub Pages has no such limit.
-      sourcemap: !process.env.CF_PAGES,
-    },
     server: {
       open: true,
     },
